7551aef8c8af87541ae867a1b1a5cf260378c0b3
Dennis Sebastian <dennissebastian88@yahoo.com>
2020-07-01 15:14:58 +0700

    Second

--- Javascript Course.js
+++ Javascript Course.js
@@ -75,6 +75,50 @@ var gloveBoxContents = myStorage.car.inside["glove box"];
 
 console.log(gloveBoxContents)
 
-Accessing Nested Arrays
+Record Collection
+var collection = {
+  "2548": {
+    "album": "Slippery When Wet",
+    "artist": "Bon Jovi",
+    "tracks": [
+      "Let it Rock",
+      "You Give Love a Bad Name"
+    ]
+  },
+  "2468": {
+    "album": "1999",
+    "artist": "Prince",
+    "tracks": [
+      "1999",
+      "Little Red Corvette"  
+    ]
+  },
+  "1245": {
+    "artist": "Robert Palmer",
+    "tracks": []
+  },
+  "5439": {
+    "album": "ABBA Gold"
+  }
+  
+};
+
+var collectioncopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value){
+  if (value === ""){
+    delete collection [id][prop];
+  }  else if (prop==="tracks"){
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value)
+  } else {
+    collection[id][prop] = value;
+  }
+  return collection;
+}
+
+console.log(updateRecords(5439,"album",""));
+
+
 
 
